ba8c74819d93c37574eb84f2753e718ac9b68e8c
caio <caio.yugo11@gmail.com>
2025-09-11 11:45:55 -0300

    style: login page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,9 +1,11 @@
 // src/app/about
+import Header from "@/components/Header";
 
 export default async function Checkout() {
     
     return(
         <div>
+            <Header></Header>
             <h1>
                 projeto para aula de prog web
             </h1>
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,37 +1,18 @@
 // src/app/login/page.tsx
-//import { loginUser } from '../actions';
 
-// This is a Server Component by default. No 'use client'!
-export default async function LoginPage() {
-  // You can do server-side work here, like checking if the user is already authenticated
-  // const session = await getSession();
-  // if (session) { redirect('/dashboard'); }
+import Header from '@/components/Header';
+import LoginForm from '@/components/LoginForm';
 
-  // This function handles the form submission. It runs on the server.
-  // async function handleLogin(formData: FormData) {
-  //   'use server'; // You can also define the action inline with 'use server'
-  //   await loginUser(formData);
-  // }
-
+export default function LoginPage() {
   return (
-    <div className="">
-      <h1>Login</h1>
-      {/* The form calls the Server Action directly */}
-      <form>
-        <div>
-          <label htmlFor="email">Email</label>
-          <input id="email" name="email" type="email" required />
+    <div className='p-6'>
+      <Header></Header>
+      <div className="min-h-screen flex items-center justify-center bg-grey-500">
+        <div className="p-8 bg-black rounded shadow w-full max-w-md">
+          <h1 className="text-2xl font-bold mb-6 text-center">Login</h1>
+          <LoginForm onSuccessRedirect="/dashboard" />
         </div>
-        <div>
-          <label htmlFor="password">Password</label>
-          <input id="password" name="password" type="password" required />
-        </div>
-        <button type="submit">Log in</button>
-      </form>
-
-      <p>
-        Don t have an account? <a href="/register">Register here</a>.
-      </p>
+      </div>
     </div>
   );
 }
