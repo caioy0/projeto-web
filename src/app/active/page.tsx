b8bb0b77f47045fc9dbe5e2d1c4ae0114172a3cc
Caio <caio.yugo@proton.me>
2025-09-18 16:47:16 -0300

    chore: disabled active component

--- src/app/active/page.tsx
+++ src/app/active/page.tsx
@@ -1,12 +1,11 @@
 // @/app/active/page.tsx
-import Active from "@/components/Active";
+// import Active from "@/components/Active";
 import Header from "@/components/Header";
 
 export default async function ActivePage(){
     return (
         <main>
             <Header></Header>
-            <Active></Active>
         </main>
     );
 }
