adcb40665e2bed64ff7400e46973fb6c6ce4b390
Caio <caio.yugo@proton.me>
2025-10-23 18:42:30 -0300

    chore: register forms

--- src/app/active/page.tsx
+++ src/app/active/page.tsx
@@ -1,11 +1,20 @@
 // @/app/active/page.tsx
-// import Active from "@/components/Active";
+
+import Active from "@/components/Confirm/Active";
 import Header from "@/components/Header";
+import { Suspense } from 'react';
 
 export default async function ActivePage(){
     return (
         <main>
-            <Header></Header>
+            <div className="">
+                <Header/>
+            </div>
+            <div>
+                <Suspense fallback={<p>Carregando página de ativação...</p>}>
+                    <Active/>
+                </Suspense>
+            </div>
         </main>
     );
 }
